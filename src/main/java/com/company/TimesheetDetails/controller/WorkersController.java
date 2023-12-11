80e2ad86fb355c64ce4bebb697cde990902971c6
anaralizada <anar_alizade@internet.ru>
2023-12-11 21:20:45 +0400

    configured

--- src/main/java/com/company/TimesheetDetails/controller/WorkersController.java
+++ src/main/java/com/company/TimesheetDetails/controller/WorkersController.java
@@ -2,6 +2,7 @@ package com.company.TimesheetDetails.controller;
 
 import com.company.TimesheetDetails.entity.Workers;
 import com.company.TimesheetDetails.service.WorkersService;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class WorkersController {
         this.workersService = workersService;
     }
 
-    @GetMapping("/workers")
+    @GetMapping(value = "/workers", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Workers> listWorkers() {
         return workersService.getAllWorkers();
     }
