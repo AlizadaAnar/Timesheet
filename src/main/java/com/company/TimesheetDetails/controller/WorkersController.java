7ef9feb603fb9ca920736750dc1ed9acc6eaf47e
anaralizada <anar_alizade@internet.ru>
2023-12-14 13:02:25 +0400

    cdhbcsjdh

--- src/main/java/com/company/TimesheetDetails/controller/WorkersController.java
+++ src/main/java/com/company/TimesheetDetails/controller/WorkersController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
-@CrossOrigin(origins = "http://localhost:5174", allowCredentials = "true", allowedHeaders = {"Authorization", "Content-Type"})
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true", allowedHeaders = {"Authorization", "Content-Type"})
 public class WorkersController {
 
     private final WorkersService workersService;
