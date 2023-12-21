886019e8a5e56b87d95af4cd3910679590e69ef9
anaralizada <anar_alizade@internet.ru>
2023-12-22 01:59:00 +0400

    lll

--- src/main/java/com/company/TimesheetDetails/TimesheetDetailsApplication.java
+++ src/main/java/com/company/TimesheetDetails/TimesheetDetailsApplication.java
@@ -2,6 +2,9 @@ package com.company.TimesheetDetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TimesheetDetailsApplication {
@@ -10,15 +13,15 @@ public class TimesheetDetailsApplication {
         SpringApplication.run(TimesheetDetailsApplication.class, args);
     }
 
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("*");
-//			}
-//		};
-//	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS").allowedOrigins("http://localhost:5175");
+			}
+		};
+	}
 
 
 }
